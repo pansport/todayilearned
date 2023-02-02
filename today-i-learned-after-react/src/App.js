2e5afe0cd926e5b8132ffcac90578220db99a769
pansport <111249835+pansport@users.noreply.github.com>
2023-02-02 19:32:36 +0100

    Add files via upload

---
+++ today-i-learned-after-react/src/App.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+function App() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="logo image" />
+        <h1>Today I Learned!</h1>
+      </div>
+      <button className="btn btn-large btn-share">Share a fact</button>
+    </header>
+  );
+}
+
+export default App;
